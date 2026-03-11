f3fc40fe55c8be41266f9a4bdf5a549b1bc59cea
Nikita Leonov <fanatbebrbl@gmail.com>
2026-03-11 08:48:12 +0300

    added login page tests

---
+++ pages/locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    INVALID_LOGIN_LINK = (By.CSS_SELECTOR, "#invalid_login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,18 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()                      
-    page.go_to_login_page()          
+    page.go_to_login_page()
+    page = LoginPage(browser, link) 
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
